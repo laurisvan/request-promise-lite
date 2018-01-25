cba98f6c3ab673278f734dd2ad25be845c73a480
Baranov <ABaranov@kiev.luxoft.com>
2018-01-25 16:30:13 +0200

    added interface with url in options object

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const lib = require('./src');
 
-module.exports = (options) => {
+module.exports = (url, options) => {
+  if (typeof url === 'object') {
+    options = url;
+  } else {
+    options.uri = url;
+  }
   const method = options.method || 'get';
   return lib[method.toLowerCase()].call(null, options.uri, options);
 };
